2cb7538f54345a985de37f919b59904fcfdffc9d
Casey Martell <drmartell@gmail.com>
2020-02-21 10:52:58 -0800

    use identity endpoint to return the username

--- oAuth/disconnect-test.js
+++ oAuth/disconnect-test.js
@@ -18,7 +18,7 @@ module.exports = require('express').Router()
       consumerKey,
       consumerSecret,
       `http://${process.env.CALLBACK_HOST}/api/v1/callback`,
-      function(err, requestData){
+      (err, requestData) => {
         // Persist "requestData" here so that the callback handler can 
         // access it later after returning from the authorize url
         oAuthData.requestData = requestData;
@@ -31,32 +31,39 @@ module.exports = require('express').Router()
     const oAuth = new Discogs(oAuthData.requestData).oauth();
     oAuth.getAccessToken(
       req.query.oauth_verifier, // Verification code sent back by Discogs
-      function(err, accessData){
+      (err, accessData) => {
         console.log('accessData', accessData);
         // Persist "accessData" here for following OAuth calls
         oAuthData.accessData = accessData;
-        res.send('Received access token!');
+        //res.send('Received access token!');
+
+        const dis = new Discogs(oAuthData.accessData);
+        dis.getIdentity((err, identity) => {
+          console.log(identity);
+          res.send(identity);
+        });
       });
   })
 
-// .get('/identity', function(req, res){
-//   const dis = new Discogs(oAuthData.accessData);
-//   dis.getIdentity(function(err, data){
-//     console.log(data);
-//     res.send(data);
-//   });
-// });
+  .get('/identity', (req, res) => {
+    const dis = new Discogs(oAuthData.accessData);
+    dis.getIdentity((err, identity) => {
+      console.log(identity);
+      res.send(identity);
+    });
+  })
 
   .get('/imagetest', function(req, res){
     const db = new Discogs(oAuthData.accessData).database();
-    db.getRelease(176126, function(err, data){
-      console.log('Image URL', data.images[0].resource_url);
-      // const url = data.images[0].resource_url;
-      // db.getImage(url, function(err, data, rateLimit){
-      //   // Data contains the raw binary image data
-      //   require('fs').writeFile('/tmp/image.jpg', data, 'binary', function(err){
-      //     console.log('Image saved!');
-      //   });
-      // });
-    });
+    db.getRelease(176126,
+      (err, data) => {
+        console.log('Image URL', data.images[0].resource_url);
+        // const url = data.images[0].resource_url;
+        // db.getImage(url, function(err, data, rateLimit){
+        //   // Data contains the raw binary image data
+        //   require('fs').writeFile('/tmp/image.jpg', data, 'binary', function(err){
+        //     console.log('Image saved!');
+        //   });
+        // });
+      });
   });
