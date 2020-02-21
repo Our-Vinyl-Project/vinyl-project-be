a7c942d30b2b17438571a3b7664810da1de3f45f
Casey Martell <drmartell@gmail.com>
2020-02-20 17:15:29 -0800

    basic oAuth loop functioning

---
+++ oAuth/disconnect-test.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const Discogs = require('disconnect').Client;
+
+const consumerKey = process.env.CONSUMER_KEY;
+const consumerSecret = process.env.CONSUMER_SECRET;
+const oAuthData = {};
+
+// const db = new Discogs().database();
+// db.getRelease(176126, function(err, data){
+//   console.log(data);
+// });
+
+module.exports = require('express').Router()
+
+  .get('/authorize', function(req, res){
+    const oAuth = new Discogs().oauth();
+    oAuth.getRequestToken(
+      consumerKey,
+      consumerSecret,
+      `http://${process.env.CALLBACK_HOST}/api/v1/callback`,
+      function(err, requestData){
+        // Persist "requestData" here so that the callback handler can 
+        // access it later after returning from the authorize url
+        oAuthData.requestData = requestData;
+        console.log('requestData', requestData);
+        res.redirect(requestData.authorizeUrl);
+      });
+  })
+
+  .get('/callback', function(req, res){
+    const oAuth = new Discogs(oAuthData.requestData).oauth();
+    oAuth.getAccessToken(
+      req.query.oauth_verifier, // Verification code sent back by Discogs
+      function(err, accessData){
+        console.log('accessData', accessData);
+        // Persist "accessData" here for following OAuth calls
+        oAuthData.accessData = accessData;
+        res.send('Received access token!');
+      });
+  })
+
+// .get('/identity', function(req, res){
+//   const dis = new Discogs(oAuthData.accessData);
+//   dis.getIdentity(function(err, data){
+//     console.log(data);
+//     res.send(data);
+//   });
+// });
+
+  .get('/imagetest', function(req, res){
+    const db = new Discogs(oAuthData.accessData).database();
+    db.getRelease(176126, function(err, data){
+      console.log('Image URL', data.images[0].resource_url);
+      // const url = data.images[0].resource_url;
+      // db.getImage(url, function(err, data, rateLimit){
+      //   // Data contains the raw binary image data
+      //   require('fs').writeFile('/tmp/image.jpg', data, 'binary', function(err){
+      //     console.log('Image saved!');
+      //   });
+      // });
+    });
+  });
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
+const express = require('express');
 require('./lib/utils/connect')();
 
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 7883;
+
+// app.use(express.json());
+// app.use('/api/v1/', require('./oAuth/disconnect-test'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
+
+module.exports = app;
