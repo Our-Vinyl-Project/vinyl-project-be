ceb8c5946e977c336428b48bce1c27fe8b908d45
okFox <smayfield@gmail.com>
2020-02-27 18:45:56 -0800

    added node cron and set schedule

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+const cron = require('node-cron');
+
 
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7891;
+
+cron.schedule('0 0 * * * ', function() {
+  console.log('cron running every day at midnight');
+});
+
+// listen for cron
+app.listen('3128', () => {
+  console.log('cron listening on 3128');
+});
 
 const server = app.listen(PORT, () => console.log(`Started on ${PORT}`)); // eslint-disable-line no-console
 server.setTimeout(240000);
