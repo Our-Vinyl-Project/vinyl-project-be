06a1bd0025be1e72f914398bbb44aa2a416107b0
okFox <smayfield@gmail.com>
2020-02-27 19:12:06 -0800

    cron job working

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 const cron = require('node-cron');
+const { getPrices } = require('./lib/utils/getPrices');
 
 
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7891;
 
-cron.schedule('0 0 * * * ', function() {
-  console.log('cron running every day at midnight');
+cron.schedule('0 0 * * * ', () => {
+  getPrices();
+  console.log('cron running every day at midnight'); // eslint-disable-line 
 });
 
-// listen for cron
-app.listen('3128', () => {
-  console.log('cron listening on 3128');
-});
+app.listen('3128');
 
 const server = app.listen(PORT, () => console.log(`Started on ${PORT}`)); // eslint-disable-line no-console
 server.setTimeout(240000);
